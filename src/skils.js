6c9ab87f5751f1f632cefa773a41d1ecda39a977
kumenger <44558777+kumenger@users.noreply.github.com>
2023-04-21 10:55:18 -0600

    first commit

---
+++ src/skils.js
@@ -0,0 +1,50 @@
+import SkillsComponent from './SkillsCOmponent';
+import React from 'react'
+const Skills=()=>{
+  return ( <div className="row aboutme ">
+
+
+    <SkillsComponent title={'FrontEnd'} skills={[
+
+       {src: 'https://img.icons8.com/color/48/000000/react-native.png', skilltype: 'React'},
+       {src: 'https://img.icons8.com/ios-filled/50/000000/javascript-logo.png', skilltype:'javascript'},
+        {src: 'https://img.icons8.com/color/48/000000/bootstrap.png', skilltype:'boostrap'},
+        {src: 'https://img.icons8.com/color/48/000000/redux.png', skilltype: 'Redux'},
+        {src: 'https://img.icons8.com/ios-filled/50/null/jquery.png', skilltype: 'jquery'},
+        {src: 'https://img.icons8.com/ios/50/null/css.png', skilltype: 'css'},
+        {src: 'https://img.icons8.com/color/48/null/material-ui.png', skilltype: 'material-ui'},
+        
+       
+        
+      
+      
+        ]}/>
+    
+    
+    <SkillsComponent title={'BackEnd'} skills={[
+      {src: 'https://img.icons8.com/color/48/000000/nodejs.png', skilltype: 'Node'}, 
+     
+     {src: 'https://img.icons8.com/color/48/null/mongoose.png', skilltype: 'mongoose'}, 
+      {src: 'https://img.icons8.com/color/48/000000/graphql.png', skilltype: 'Grapgiql'},
+    
+      {src: 'https://img.icons8.com/nolan/64/express-js.png', skilltype: 'Express.js'},
+      {src: 'https://img.icons8.com/ios/50/null/api-settings.png', skilltype: 'API'}
+      
+      
+      ]}/>{"  "}
+        <SkillsComponent title={'DataBase and more...'} skills={[
+        {src: 'https://img.icons8.com/ios-filled/50/null/mysql.png', skilltype: 'Mysql'},
+        {src: 'https://img.icons8.com/color/48/000000/mongodb.png', skilltype: 'MongoDb'},
+        {src: 'https://img.icons8.com/color/48/000000/json--v1.png', skilltype: 'Json'}, 
+        {src: 'https://img.icons8.com/color/48/000000/git.png', skilltype: 'Git'},
+        {src: 'https://img.icons8.com/color/48/000000/amazon-web-services.png', skilltype: 'AWS'},
+        {src: 'https://img.icons8.com/ios-filled/50/null/github.png', skilltype: 'GitHub'}
+     
+      ]}/>
+    
+   
+<a className='text-light' href='https://icons8.com/' target="_black">Thanks to Icons'8 </a>
+
+  </div>);
+};
+export default Skills;
